5f748f419868e752d799f44c2bfcdf7d68fb2e27
neelsshah2006 <neelsshah2006@gmail.com>
2025-10-29 22:23:55 +0530

    Problem 1368/A done

---
+++ A1368.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class A1368 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+
+            int count = 0;
+            while (a <= n && b <= n) {
+                if (a < b) {
+                    a += b;
+                } else {
+                    b += a;
+                }
+                count++;
+            }
+            System.out.println(count);
+        }
+        sc.close();
+    }
+}
