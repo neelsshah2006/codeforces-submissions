665bdda5707b03cce68c5baa3b61f072091bef94
neelsshah2006 <neelsshah2006@gmail.com>
2025-11-05 10:43:25 +0530

    Problem 1866/A solved

---
+++ A1866.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class A1866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            int a = Math.abs(sc.nextInt());
+            if (a < min) {
+                min = a;
+            }`
+        }
+        sc.close();
+        System.out.println(min);
+    }
+}
