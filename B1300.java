93f25786799659ab26bcd6a046193dcee803a776
neelsshah2006 <neelsshah2006@gmail.com>
2025-11-07 17:56:47 +0530

    Problem 1300/B solved

---
+++ B1300.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B1300 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[2 * n];
+            for (int i = 0; i < 2 * n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Arrays.sort(arr);
+            System.out.println(Math.abs(arr[n] - arr[n - 1]));
+        }
+        sc.close();
+    }
+}
